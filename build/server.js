1d247b39ee04f26732b413232bd627efed3ef34f
derac <dwolson123@gmail.com>
2021-07-19 13:08:00 -0500

    add get types endpoint

--- build/server.js
+++ build/server.js
@@ -5,9 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
+var PAT = "testtokenabcdefg";
 app.get('/', function (req, res) {
     res.send('Well done!');
 });
+app.get('/v4/linode/types', function (req, res) {
+    res.sendFile('Types-Response.txt', { "root": "./assets" });
+});
+// create token - do we need this?
+// app.post('/v4/profile/tokens', (req, res) => {
+//     res.send()
+// })
 app.listen(3000, function () {
     console.log('The application is listening on port 3000!');
 });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 
 const app = express();
+const PAT = "testtokenabcdefg"
+// create token - do we need this?
+// app.post('/v4/profile/tokens', (req, res) => {
+//     res.send()
+// })
 
-app.get('/', (req, res) => {
-    res.send('Well done!');
+
+app.get('/v4/linode/types', (req, res) => {
+    res.sendFile('Types-Response.txt', {"root": "./assets"})
 })
+
+
+
 
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
