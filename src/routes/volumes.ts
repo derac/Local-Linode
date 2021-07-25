459cd10b7d107cbe9e0e51e0fdec883d2200d98d
derac <dwolson123@gmail.com>
2021-07-25 18:16:43 -0500

    beginning volumes api create and list

--- src/routes/volumes.ts
+++ src/routes/volumes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import Docker from "dockerode";
 
+import regions from "../data/regions.json";
+
 const router = express.Router();
 const docker = new Docker();
 
@@ -8,10 +10,35 @@ const docker = new Docker();
 // /v4/volumes
 
 // Volumes List
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  docker.listVolumes().then(() => {
+    //res.send(a);
+  });
+});
 
 // Volume Create
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  let config_id: number | null,
+    label: string,
+    linode_id: number | null,
+    region: string | null,
+    size: number,
+    tags: string[] | null;
+  let datetime = new Date().toISOString();
+  // optional, size in GB defaults to 20
+  size = req.headers.size ? parseInt(req.headers.size as string) : 20;
+  // optional, linode id to attach to
+  linode_id = req.headers.linode_id
+    ? parseInt(req.headers.linode_id as string)
+    : null;
+  // optional, label will be created if not supplied
+  label = req.headers.label
+    ? (req.headers.label as string)
+    : [...Array(32)].map(() => (~~(Math.random() * 36)).toString(36)).join("");
+  docker.createVolume({ name: label }).then(() => {
+    res.send(`${size}, ${label}`);
+  });
+});
 
 // Volume Delete
 router.delete("/:volumeId", (req, res) => {});
