84f6857c31d75f35c60a06b22b67c0b58d385244
derac <dwolson123@gmail.com>
2021-07-27 18:04:52 -0500

    add return before res send statements

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -9,16 +9,16 @@ const router = express.Router();
 
 // Types List
 router.get("/", (req, res) => {
-  res.send(types);
+  return res.send(types);
 });
 
 // Type View
 router.get("/:typeId", (req, res) => {
   let typeData = types.data.filter((type) => type.id == req.params.typeId);
   if (typeData.length) {
-    res.send(typeData);
+    return res.send(typeData);
   } else {
-    res.status(404).json({ errors: [{ reason: "Not found" }] });
+    return res.status(404).json({ errors: [{ reason: "Not found" }] });
   }
 });
 
