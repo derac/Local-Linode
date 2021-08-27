a6a23b2030ed04dcf0592f5eba787d562716c718
derac <dwolson123@gmail.com>
2021-08-27 05:40:51 -0500

    sorted out bug in initializing database, wasn't being written correctly

--- src/setup/initial.ts
+++ src/setup/initial.ts
@@ -4,16 +4,14 @@ import path from "path";
 import sqlite3 from "sqlite3";
 
 // make database if it doesn't exist
-const db_file = path.join(__dirname, "../data/database.sqlite3");
-if (!fs.existsSync(db_file)) {
-  fs.openSync(db_file, "w");
-  const db = new sqlite3.Database(db_file);
-  db.run(
-    'CREATE TABLE "volumes" ("id"	TEXT NOT NULL UNIQUE, "data"	JSON NOT NULL, PRIMARY KEY("id") );'
-  );
-  db.run(
-    'CREATE TABLE "instances" ("id"	TEXT NOT NULL UNIQUE, "data"	JSON NOT NULL, PRIMARY KEY("id") );'
-  );
-}
+const db_file = path.join(__dirname, "..", "data", "database.sqlite3");
+const db = new sqlite3.Database(db_file);
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS volumes (id text PRIMARY KEY, data json NOT NULL );"
+);
+db.run(
+  "CREATE TABLE IF NOT EXISTS instances (id text PRIMARY KEY, data json NOT NULL );"
+);
 
 export default {};
